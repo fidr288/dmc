8170acb5f91e66ce60d4c3c452da4fc3ada21c65
fidr <f1gp3a@gmail.com>
2020-05-05 02:10:47 +0000

    update

---
+++ js/form.js
@@ -0,0 +1,51 @@
+function resident() {
+  let contact = document.querySelector('input[name="contact"]:checked').value
+  var resident = document.getElementById("resident");
+  var contactbox = document.getElementById("contact");
+  var days = document.getElementById("days");
+  var notresident = document.getElementById("notresident");
+  var noteligible = document.getElementById("noteligible");
+  // If the checkbox is checked, display the output text
+  if (contact == "yes"){
+    resident.style.display = "block";
+    noteligible.style.display ="none";
+  } else {
+    resident.style.display = "none";
+    noteligible.style.display ="block";
+    days.style.display = "none";
+    notresident.style.display ="none";
+    contactbox.style.display ="none";
+  }
+
+} 
+
+function days() {
+  let residentChoice = document.querySelector('input[name="resident"]:checked').value
+  var days = document.getElementById("days");
+  var notresident = document.getElementById("notresident");
+
+  if (residentChoice == "yes"){
+    days.style.display = "block";
+    notresident.style.display ="none";
+} 
+  else {
+    days.style.display = "none";
+    notresident.style.display ="block";
+
+  }
+} 
+
+function final() {
+    var dayChoice = document.querySelector('input[name="day"]:checked').value
+    var notresident = document.getElementById("notresident");
+    var isresident = document.getElementById("isresident");
+  
+    if (dayChoice == "yes"){
+      notresident.style.display ="none";
+      isresident.style.dispay ="block";
+  } 
+    else {
+      notresident.style.display ="block";
+  
+    }
+  } 
